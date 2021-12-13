c64aca558e70c2ee117146d3f0bc8550dda72d1b
Geoffrey Morizot <gffr.mrzt@gmail.com>
2021-12-13 17:37:36 +0100

    feat: findOne, update, delete argonaute

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5050;
 require("./models/dbConfig");
 
 const argonauteRoutes = require("./routes/argonauteController");
--- routes/argonauteController.js
+++ routes/argonauteController.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const ObjectID = require("mongoose").Types.ObjectId;
 
 const { ArgonauteModel } = require("../models/ArgonauteModel");
 
@@ -7,6 +8,15 @@ router.get("/", (req, res) => {
   ArgonauteModel.find((err, docs) => {
     if (!err) res.send(docs);
     else console.log("Error to get data" + err);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) res.status(400).send("ID unknow: " + req.params.id);
+
+  ArgonauteModel.findById(req.params.id, (err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Get error: " + err);
   });
 });
 
@@ -19,6 +29,27 @@ router.post("/", async (req, res) => {
   await argonaute.save((err, docs) => {
     if (!err) res.send(docs);
     else console.log("Error creating new Argonaute: " + err);
+  });
+});
+
+router.put("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) res.status(400).send("ID unknow: " + req.params.id);
+
+  const argonauteUpdate = {
+    name: req.body.name,
+    age: req.body.age,
+  };
+  ArgonauteModel.findByIdAndUpdate(req.params.id, { $set: argonauteUpdate }, { new: true }, (err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Update error: " + err);
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) res.status(400).send("ID unknow: " + req.params.id);
+  ArgonauteModel.findByIdAndRemove(req.params.id, (err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Delete error: " + err);
   });
 });
 
