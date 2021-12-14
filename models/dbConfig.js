b7a88f0d1285e67b446e7b82d860204b3c4287a3
Geoffrey Morizot <gffr.mrzt@gmail.com>
2021-12-14 15:42:58 +0100

    refactor: change variable env name

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if (!err) console.log("Mongodb connected !");
   else console.log("Connection error: " + err);
 });
